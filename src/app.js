adc3719fccbcaeba30396b7f2930a554921c7b0f
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-07 11:00:53 +0300

    modified to add the recipe rout

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const db_connect = require('./db/database')
 const userRoutes = require('./routes/userRoutes')
 const authRoutes = require('./routes/authRoutes')
+const recipeRoutes = require('./routes/recipeRoute')
 
 const app = express()
 app.use(cors())
@@ -17,6 +18,7 @@ db_connect().then(() => {
 app.use(express.json())
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/recipe', recipeRoutes)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
