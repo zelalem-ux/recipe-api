856c9b64cc4bf51b4c6d0da3386131851cc21ec2
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-07 10:34:17 +0300

    adding timestamps for the recipe model

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
 
 // Define the Schema for the Recipe model
-const recipeSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  ingredients: [{ type: String, required: true }],
-  instructions: [{ type: String, required: true }],
-  image: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date },
-})
+const recipeSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    ingredients: [{ type: String, required: true }],
+    instructions: [{ type: String, required: true }],
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+)
 
 const RecipeModel = mongoose.model('Recipe', recipeSchema)
 
